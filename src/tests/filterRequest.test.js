44b917758ede09fbcf2a5e8352742be5fadeb39b
Ovy95 <jackoverton21@hotmail.com>
2021-06-07 16:36:22 +0100

    No repos Function is tested using mocked Data

--- src/tests/filterRequest.test.js
+++ src/tests/filterRequest.test.js
@@ -1,14 +1,13 @@
 import {filterDataByLanguages,FavouriteLanguage,noRepos,filterToFavouriteLanguage} from "../components/filterRequest"
+import mockJsonData from './mockdataJson'
 
+describe('noRepos Function, returns boolean values checking if data passed is empty', () => {
+  it('Returns true', () => {
+    let data = {}
+    expect(noRepos(data)).toBe(true)
+  })
+  it('Returns false', () => {
+    expect(noRepos(mockJsonData)).toBe(false)
+  })
 
-describe('noRepos Function, returns boolean values checking empty data', () => {
- it('Returns false', () => {
-   let data = {}
-   expect(noRepos(data)).toBe(true)
- })
-
-//  it('Returns true', () => {
-//   let data = {}
-//   expect(noRepos(data)).toBe(false)
-// })
 })
---
+++ src/tests/mockdataJson.js
@@ -0,0 +1,24 @@
+export default 
+
+[ {
+  "language": null
+},
+{
+  "language": "Ruby"
+},
+{
+  "language": "GoLang"
+},
+{
+  "language": "JavaScript"
+},
+{
+  "language": "JavaScript"
+},
+{
+  "language": "JavaScript"
+},
+{
+  "language": "Ruby"
+}
+]
